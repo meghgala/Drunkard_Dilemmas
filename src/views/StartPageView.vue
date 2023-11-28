a9eda5a77a1084fe3bca6682fbb074a911ed40a1
ThereseBjorkman <therese.bjorkman@outlook.com>
2023-11-28 12:31:12 +0100

    startpage

--- src/views/StartPageView.vue
+++ src/views/StartPageView.vue
@@ -3,15 +3,16 @@
         <header>
             <h1>Drunkard Dilemmas</h1>
         </header>
+        <div class="Flag-Button">
+                <button class="flag1"></button>
+                <button class="flag2"></button>
+        </div>
         <section class="Buttons">
             <div class="Button-Create">
                 <button>Create</button>
             </div>
             <div class="Button-Join">
                 <button>Join</button>
-            </div>
-            <div class="Button-Language">
-                <button>Change language</button>
             </div>
         </section>
     </body>
@@ -21,6 +22,13 @@
 
 body{
     background-color:rgb(34, 5, 54);
+    font-family: 'Impact', sans-serif;
+    color: aliceblue;
+    font-size: xx-large;
+}
+
+header{
+    padding: 3em;
 }
 
 .Buttons{
@@ -39,10 +47,47 @@ body{
     justify-self: start;
 }
 
-.Button-Language{
+button {
+      margin: 1vh 2vh;
+      height: 7em;
+      width: 15em;
+      border: transparent;
+      cursor: pointer;
+      transition: background-color 0.3s;
+      text-align: center;
+      border-radius: 20px;
+      font-size: 25px;
+      font-weight: bolder;
+}
+
+button:hover {
+    background-color:seagreen;
+}
+
+.Flag-Button {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
     grid-column: 1 / span 2;
-    grid-row: 1;
-    justify-self: end;
+}
+
+.flag1, .flag2 {
+    position: absolute;
+    width: 100%;
+    height: 100%; 
+    background-size: cover;
+}
+
+.flag1 {
+  clip-path: polygon(0% 0%, 100% 0%, 0% 100%);
+  background-image: url('https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53E4/A986/7055/D0CD/B15F/0A28/100A/F37A/Flag_of_Sweden_ml.png');
+  transform: scale(0.15);
+}
+
+.flag2 {
+  clip-path: polygon(100% 0%, 100% 100%, 0% 100%);
+  background-image: url('https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53E6/2F6A/4819/C919/633F/0A28/100B/048A/Flag_of_the_United_Kingdom.png');
+  transform: scale(0.15);
 }
 </style>
 
