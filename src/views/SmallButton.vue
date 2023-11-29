b46d5468ca6a6d13c52ec74ebd05fe91a8699aab
Johan Sjöborg <johan.sjoborg.2167@student.uu.se>
2023-11-29 08:28:17 +0100

    started on button

---
+++ src/views/SmallButton.vue
@@ -0,0 +1,35 @@
+
+
+<template>
+  <button :style="{ backgroundColor: bgColor }" @click="handleClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'blue',
+    },
+  },
+  data() {
+    return {
+      bgColor: 'red',
+    };
+  },
+  methods: {
+    handleClick() {
+      this.bgColor = this.color;
+      this.$emit('click');
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  padding: 5px 10px;
+}
+</style>
