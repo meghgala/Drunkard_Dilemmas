49b253e819e3719306974eadb1cf0491781e0451
ThereseBjorkman <therese.bjorkman@outlook.com>
2023-12-01 13:07:49 +0100

    making roomcode stuff

--- src/views/JoinRoomView.vue
+++ src/views/JoinRoomView.vue
@@ -4,11 +4,6 @@
     </header>
     <body>
         <section class="Buttons">
-            <router-link  to="/select/" custom v-slot="{ navigate }">
-              <button class="Button-Create" @click="navigate" role="link">
-                  {{ uiLabels.createPoll }}
-              </button>
-            </router-link>
             <router-link  to="/join/" custom v-slot="{ navigate }">
               <button class="Button-Join" @click="navigate" role="link">
                   {{ uiLabels.participatePoll }}
@@ -20,6 +15,38 @@
 
 
 <script>
+
+import io from 'socket.io-client';
+const socket = io("localhost:3000");
+
+export default {
+  name: 'StartView',
+  data: function () {
+    return {
+      uiLabels: {},
+      id: "",
+      lang: localStorage.getItem("lang") || "en",
+    }
+  },
+  created: function () {
+    socket.emit("pageLoaded", this.lang);
+    socket.on("init", (labels) => {
+      this.uiLabels = labels
+    })
+  },
+  methods: {
+    switchLanguage: function() {
+      if (this.lang === "en") {
+        this.lang = "sv"
+      }
+      else {
+        this.lang = "en"
+      }
+      localStorage.setItem("lang", this.lang);
+      socket.emit("switchLanguage", this.lang)
+    }
+  }
+}
 </script>
 
 
@@ -36,5 +63,26 @@ header{
     font-size: 4vmin;
     padding: 2em;
 }
+
+.Button-Join{
+    grid-column: 2;
+    grid-row: 2;
+    justify-self: start;
+    background-color: #FF5929;
+}
+
+button {
+    margin: 1vh 1vh;
+    height: 15vh;
+    width: 15vw;
+    border: transparent;
+    cursor: pointer;
+    transition: background-color 0.3s;
+    text-align: center;
+    border-radius: 20pt;
+    font-size: 5vmin;
+    font-weight: bolder;
+}
+
 </style>
 
