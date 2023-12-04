c4caf3c12cba21ebdefa1025f55a0425a25f8344
Megh Gala <megh.gala765@gmail.com>
2023-12-04 17:37:02 +0100

    particles added

---
+++ src/components/Particlesvue.vue
@@ -0,0 +1,102 @@
+<script setup>
+import { loadFull } from "tsparticles";
+
+const particlesInit = async (engine) => {
+  await loadFull(engine);
+};
+
+const particlesLoaded = async (container) => {
+  console.log("Particles container loaded", container);
+};
+
+</script>
+
+<template>
+  <div>
+    <Particles
+      id="tsparticles"
+      :particlesInit="particlesInit"
+      :particlesLoaded="particlesLoaded"
+      :options="{
+        background: {
+          color: {
+            value: '#160a3d',
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: 'push',
+            },
+            onHover: {
+              enable: true,
+              mode: 'grab',
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: '#00c8c1', // Neon color
+          },
+          links: {
+            enable: false, // Disable links to remove lines
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outMode: 'bounce',
+            random: false,
+            speed: 5,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 20,
+          },
+          opacity: {
+            value: 5,
+          },
+          shape: {
+            type: 'circle',
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }"
+      style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: -1;"
+    />
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
