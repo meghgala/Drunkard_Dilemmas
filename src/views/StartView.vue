2c8fe733a3d8108c3b8f6cd5c1eb29fcf5496896
Megh Gala <megh.gala765@gmail.com>
2024-01-15 18:12:12 +0100

    Rolled back to functional code but only locally

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -42,8 +42,8 @@
   import io from 'socket.io-client';
   import Particlesvue from '@/components/Particlesvue.vue'
   
-  sessionStorage.setItem("dataServer", "https://spiffy-semolina-1a2e9e.netlify.app/")
-  // sessionStorage.setItem("dataServer", "130.243.153.58:3000")
+  //sessionStorage.setItem("dataServer", "https://spiffy-semolina-1a2e9e.netlify.app/")
+  sessionStorage.setItem("dataServer", "172.16.3.158:3000")
   const socket = io(sessionStorage.getItem("dataServer"));
   
   export default {
